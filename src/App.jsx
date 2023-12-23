c4cb125fad8b15444a3fed14a17f48bd9948d5fb
JakeZook <jakezook01@gmail.com>
2023-12-22 22:44:16 -0700

    Added review card templates.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import "./App.css";
 import Nav from "./componets/Nav";
 import Home from "./Pages/Home";
 import EditGms from "./Pages/EditGms";
+import Reviews from "./Pages/Reviews";
 
 function App() {
 	return (
 		<div>
 			<Nav />
-			<EditGms />
+			<Reviews />
 		</div>
 	);
 }
